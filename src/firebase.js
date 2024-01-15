e895d69b36acc549ba1daf9433090fdfd92eb322
skybluesharkk <104782301+skybluesharkk@users.noreply.github.com>
2024-01-15 21:14:16 +0900

    init3

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+import firebase from "firebase/app"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyArLF3-l_Bna532r5Bt7U1YqKXHF1GO0MY",
+    authDomain: "nwitter-85122.firebaseapp.com",
+    projectId: "nwitter-85122",
+    storageBucket: "nwitter-85122.appspot.com",
+    messagingSenderId: "696994947545",
+    appId: "1:696994947545:web:40ab315424f06763a1b9c8",
+    measurementId: "G-J276ECWGJT"
+  };
